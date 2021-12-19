b4271a81f41ef0cdef3a2927355be10a4efcbc63
Mahmud-Alam <official.mahmud23@gmail.com>
2021-12-19 19:25:29 +0600

    add unauthorized page template

--- users/decorators.py
+++ users/decorators.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import decorators
-from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
@@ -21,7 +20,8 @@ def allowed_users(allowed_roles=[]):
             if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
 
-            else: return HttpResponse('You are not authorized to view this page')
+            else: 
+                return render(request,'users/unauthorized_page.html')
 
         return wrapper_func
     return decorator
